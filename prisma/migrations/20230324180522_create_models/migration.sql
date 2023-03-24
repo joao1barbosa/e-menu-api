492bc374793d23f5d21c4d00997ef2442a39aa1d
joao1-barbosa <joao.rbarbosa3@gmail.com>
2023-03-24 15:16:26 -0300

    prisma migrate and create user module

---
+++ prisma/migrations/20230324180522_create_models/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "restaurantName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "picture" TEXT,
+    "restaurantID" INTEGER NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_restaurantName_key" ON "User"("restaurantName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_restaurantID_fkey" FOREIGN KEY ("restaurantID") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/modules/routes.js
+++ src/modules/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { ProductRouter } from './products/router';
+import { UserRouter } from './user/router';
 
 const routes = Router();
 
 routes.use('/product', ProductRouter);
+routes.use('/user', UserRouter);
 
 export default { routes };
